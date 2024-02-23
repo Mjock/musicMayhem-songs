fbff11cf3a63999dc74e8e0d05c2e025a7e19a37
Mjock <syedmpeerzade07@gmail.com>
2024-02-23 21:56:40 +0530

    final commit

--- TuneHubApp/src/test/java/com/main/project/MusicMayhemApplicationTests.java
+++ TuneHubApp/src/test/java/com/main/project/MusicMayhemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TuneHubAppApplicationTests {
+class MusicMayhemApplicationTests {
 
 	@Test
 	void contextLoads() {
